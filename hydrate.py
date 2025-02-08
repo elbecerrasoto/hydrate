3646554171fd9fbb486a2a80365b2ae39e44c9b8
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-02-08 11:59:51 -0700

    wip

--- hydrate.py
+++ hydrate.py
@@ -5,12 +5,15 @@ from multiprocessing import Pool
 import pandas as pd
 import numpy as np
 import shlex
+import os
+from pathlib import Path
 
 IN = "genomes.tsv"
 TRIES = 12
 BATCH_SIZE = 4
 VERBOSE = True
 KEY = "80e90a387605463df09ac9121d0caa0b7108"
+WORKERS = os.cpu_count() * 4
 
 DEHYDRATE_LEAD = ["datasets", "download", "genome", "accession"]
 DEHYDRATE_LAG = ["--dehydrated", "--include", "protein,gff3", "--api-key", f"{KEY}"]
@@ -20,14 +23,18 @@ REHYDRATE_LEAD = ["datasets", "rehydrate", "--api-key", f"{KEY}"]
 def worker(batch):
     idx, genomes = batch
 
-    dehydrated_zip = f"genomes/{idx}/{idx}.zip"
-    rehydrated_dir = f"genomes/{idx}"
+    dehydrated_zip = Path(f"genomes/{idx}/{idx}.zip")
+    rehydrated_dir = Path(f"genomes/{idx}")
+    rehydrated_dir.mkdir(parents=True)
 
     dehydrate_cmd = (
-        DEHYDRATE_LEAD + list(genomes) + ["--filename", dehydrated_zip] + DEHYDRATE_LAG
+        DEHYDRATE_LEAD
+        + list(genomes)
+        + ["--filename", str(dehydrated_zip)]
+        + DEHYDRATE_LAG
     )
-    unzip_cmd = ["unzip", dehydrated_zip, "-d", rehydrated_dir]
-    rehydrate_cmd = REHYDRATE_LEAD + ["--directory", rehydrated_dir]
+    unzip_cmd = ["unzip", str(dehydrated_zip), "-d", str(rehydrated_dir)]
+    rehydrate_cmd = REHYDRATE_LEAD + ["--directory", str(rehydrated_dir)]
 
     return dehydrate_cmd, dehydrate_cmd, unzip_cmd
 
@@ -40,7 +47,7 @@ if __name__ == "__main__":
     batches = np.array_split(genomes, int(np.ceil(len(genomes) / BATCH_SIZE)))
     batches = tuple(enumerate(batches))
 
-    with Pool() as p:
+    with Pool(WORKERS) as p:
         results = p.map(worker, batches)
 
     print(results)
